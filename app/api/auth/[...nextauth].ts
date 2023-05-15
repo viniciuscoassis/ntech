7773897f68c8c28ee6d103214e274508959d2fca
vinicius <vinicius.coassis@gmail.com>
2023-05-15 08:29:54 -0300

    feat: add authorize  with next auth

--- app/api/auth/[...nextauth].ts
+++
@@ -1,6 +0,0 @@
-import bcrypt from 'bcrypt'
-import NextAuth from 'next-auth/next'
-import CredentialsProvider from 'next-auth'
-import { PrismaAdapter } from '@next-auth/prisma-adapter'
-
-import prisma from '@/libs/prismadb'
