7db0700b3ac25eaf937d4db4de431f36b83d33e2
vinicius <vinicius.coassis@gmail.com>
2023-05-29 08:12:42 -0300

    feat: add responsive client

--- app/client/page.tsx
+++ app/client/page.tsx
@@ -11,15 +11,15 @@ export default function Client() {
   }, []);
   return (
     <div className='h-full flex justify-center items-center w-full'>
-      <div className='text-slate-600 mx-auto w-1/2 h-4/6 flex flex-col justify-center mb-20 bg-white rounded-lg shadow-lg items-center lg:w-10/12'>
+      <div className='text-slate-600 mx-auto h-4/6 flex flex-col justify-center mb-20 bg-white rounded-lg shadow-lg items-center sm:w-full lg:w-10/12'>
         <div className='flex flex-col justify-between items-center h-1/3'>
           <h1 className=' font-bold text-center text-3xl lg:text-5xl'>
             N-GATEWAY
           </h1>
           <p className='text-1xl lg:text-2xl text-center'>
-            Faça cadastro para geração de script de configuração
+            Faça cadastro para geração de script de configuração do seu equipamento
           </p>
-          <button className='bg-slate-600 text-white w-3/6 h-12 rounded hover:scale-110 hover:bg-slate-500 transition-all'>
+          <button onClick={()=> router.push('/client/infraestrutura')} className='bg-slate-600 text-white w-3/6 h-12 rounded hover:scale-110 hover:bg-slate-500 transition-all'>
             Iniciar
           </button>
         </div>
