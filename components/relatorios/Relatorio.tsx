9d3f1a9fc1c33c13ea543cd82e9e8557521d290e
vinicius <vinicius.coassis@gmail.com>
2023-05-29 08:05:22 -0300

    feat: format date em relatórios

--- components/relatorios/Relatorio.tsx
+++ components/relatorios/Relatorio.tsx
@@ -1,12 +1,21 @@
 import { Relatorio } from "@/app/context/RelatorioContext";
 
-
+import dayjs from "dayjs";
+import { useEffect } from "react";
 
 const Relatorio = ({name, date, message} : Relatorio) => {
-    return  <div className="h-24 border-b-2 flex justify-between items-center p-10">
-            <div>{name}</div>
-            <div><div className="font-bold">{date.toString()}</div><div>19:19</div></div>
-            <div className="max-w-xs">{message}</div>
+
+const day = dayjs(date).format('DD/MM/YYYY');
+const time = dayjs(date).format('HH:mm');
+
+useEffect(()=> {
+  console.log(time);
+}, [])
+
+    return  <div className="h-24 border-b-2 flex justify-between items-center p-10 ">
+            <div className="w-1/4">{name}</div>
+            <div className=" w-1/4 "><div className="font-bold">{day}</div><div>{time}</div></div>
+            <div className="w-1/6 ">{message}</div>
           </div>
 }
 
