13a7ab05b846b05707011bb09f9036360b06529d
vinicius <vinicius.coassis@gmail.com>
2023-06-01 17:13:54 -0300

    feat: add layout da página de segurança

--- app/client/seguranca/page.tsx
+++ app/client/seguranca/page.tsx
@@ -1,8 +1,27 @@
+import { FaUserCircle } from "react-icons/fa"
+
 export default function Seguranca(){
     return (
       <>
         <h1 className=' text-5xl font-bold mb-10'>Segurança</h1>
         <h2 className='text-3xl mb-5'>Gerenciar permissões:</h2>
+      <div className="lg:w-1/2 ">
+          <div className="flex flex-col mb-10">
+            <input placeholder='Adicionar pessoas à dependencia da empresa'></input>
+            <button className="bg-primary text-white text-center">Adicionar</button>
+          </div>
+          <div className="flex justify-between items-center border-b-2 h-20 ">
+            <div className="flex items-center mr-5 ">
+              <FaUserCircle size={40} />
+              <div className="ml-5">vinicius</div>
+            </div>
+            <select>
+              <option>administrador</option>
+              <option>editor</option>
+              <option>leitor</option>
+            </select>
+          </div>
+        </div>
       </>
     );
 }
--- components/condominios/Condominio.tsx
+++ components/condominios/Condominio.tsx
@@ -5,28 +5,35 @@ import useAccountModal from "@/hooks/useAccountModal";
 import { useContext } from "react";
 import { ScriptContext } from "@/app/context/Script";
 
-const Condominio = (body : condominio) => {
+const Condominio = (body: condominio) => {
   const accountModal = useAccountModal();
-  const {script, setScript} = useContext(ScriptContext);
-    return (
-      <>
-        {' '}
-        <div className='bg-white shadow-lg p-10 mr-5 mb-5 rounded-lg w-96 h-96 flex flex-col justify-between items-center '>
-          <FaCity size={60} color='#082F49' />
-          <div className=' w-full flex flex-col items-center'>
-            <div className='w-full flex justify-between'>
-              <div className=''>Estado</div> <div>{body.estado}</div>
-            </div>
-            <div className='w-full flex justify-between'>
-              <div className=''>Cidade</div> <div>{body.cidade}</div>
-            </div>{' '}
-            <div className='w-full flex justify-between'>
-              <div className=''>Base</div> <div>{body.base}</div>
-            </div>{' '}
-          </div>
-          <Button onClick={()=> {setScript({base: body.base, city: '17'});accountModal.onOpen()}} label={'Gerar script'} />
+  const { script, setScript } = useContext(ScriptContext);
+
+  const submit = () => {
+    const extractedCity = body.cidade.split("-")[0].trim();
+    setScript({ base: body.base, city: extractedCity });
+    accountModal.onOpen();
+  }
+
+return (
+  <>
+    {' '}
+    <div className='bg-white shadow-lg p-10 mr-5 mb-5 rounded-lg w-96 h-96 flex flex-col justify-between items-center '>
+      <FaCity size={60} color='#082F49' />
+      <div className=' w-full flex flex-col items-center'>
+        <div className='w-full flex justify-between'>
+          <div className=''>Estado</div> <div>{body.estado}</div>
         </div>
-      </>
-    );
+        <div className='w-full flex justify-between'>
+          <div className=''>Cidade</div> <div>{body.cidade}</div>
+        </div>{' '}
+        <div className='w-full flex justify-between'>
+          <div className=''>Base</div> <div>{body.base}</div>
+        </div>{' '}
+      </div>
+      <Button onClick={submit} label={'Gerar script'} />
+    </div>
+  </>
+);
 }
 export default Condominio
